a85350a876b6e4a487bbd03d0260501b54194766
chepyego <jchepyego@gmail.com>
2024-05-17 02:42:19 +0300

    add pictures and services

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
   def home
     @image_paths = Dir.glob("#{Rails.root}/app/assets/images/logos1/*.png")
 
+    @happy_clients = Dir.glob("#{Rails.root}/app/assets/images/happy/*.jpg")
   end
 end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,16 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+ 
+const buttonMore = document.querySelector(".more-button");
+
+buttonMore.addEventListener("click", clickButton);
+
+function clickButton(){
+
+    let newP = document.getElementById("content-div")
+    let contP = document.createElement("P")
+    contP.innerHTML = "We ensure that our clients are satified and you get value for money"
+
+    newP.appendChild(contP);
+}
